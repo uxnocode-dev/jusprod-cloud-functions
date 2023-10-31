17f26bc34f7ee6b762d9f7271926cac970cbd5a5
Pedro Silva <pedro.silva-dev@hotmail.com>
2023-10-31 20:33:39 -0300

    feat: init

---
+++ functions/index.js
@@ -0,0 +1,553 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+const axios = require("axios");
+
+const { equilizeUserBaseMock } = require("./mock/equilize-user-base.mock");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+//#region utils
+const handleApiCall = (endpoint, token, method = "get", body) => {
+  const headers = { headers: { Authorization: `Bearer ${token}` } };
+
+  if (method === "get") return axios.get(endpoint, headers);
+  else return axios.post(endpoint, body, headers);
+};
+
+const getConfig = async () => {
+  const data = await getCollectionData("CloudFunctionConfig");
+  const getConfigByKey = (key) => data.find((item) => item.key === key).value;
+
+  return {
+    api_v1: getConfigByKey("escavador_v1"),
+    api_v2: getConfigByKey("escavador_v2"),
+    api_token: getConfigByKey("escavador_token"),
+  };
+};
+
+const removeDuplicate = (list, key) => {
+  const seen = new Set();
+
+  return list.filter((el) => {
+    const duplicate = seen.has(el[key]);
+    seen.add(el[key]);
+    return !duplicate;
+  });
+};
+
+const getUserOABProcessV1 = async (
+  url = "",
+  state = "",
+  number = "",
+  items = []
+) => {
+  try {
+    const { api_v1, api_token } = await getConfig();
+    const endpoint = url || `${api_v1}/oab/${state}/${number}/processos?page=1`;
+    const { data } = await handleApiCall(endpoint, api_token);
+
+    logger.log(
+      `Progress OAB v1: ${number} | ${data.paginator.current_page} of ${data.paginator.total_pages}`,
+      { structuredData: true }
+    );
+
+    if (data?.links?.next)
+      return await getUserOABProcessV1(
+        data?.links?.next,
+        state,
+        number,
+        data?.items
+      );
+    else return items;
+  } catch (error) {
+    logger.error("+++ EQUALIZE USER BASE +++", error, { structuredData: true });
+    throw error;
+  }
+};
+
+const getUserOABProcessV2 = async (
+  url = "",
+  state = "",
+  number = "",
+  items = []
+) => {
+  try {
+    const { api_v2, api_token } = await getConfig();
+    const endpoint =
+      url ||
+      `${api_v2}/advogado/processos?oab_estado=${state}&oab_numero=${number}`;
+    const { data } = await handleApiCall(endpoint, api_token);
+
+    logger.log(`Progress OAB v2: ${number}`, { structuredData: true });
+
+    if (data?.links?.next)
+      return await getUserOABProcessV2(data?.links?.next, state, number, [
+        ...items,
+        ...data?.items,
+      ]);
+    else return items;
+  } catch (error) {
+    logger.error("+++ EQUALIZE USER BASE +++", error, { structuredData: true });
+    throw error;
+  }
+};
+
+const buildUserProcessList = async (documents) => {
+  try {
+    const result = [];
+
+    for await (let item of documents) {
+      const itemsV2 = await getUserOABProcessV2("", item.state, item.number);
+      result.push(...itemsV2);
+    }
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const buildUserProcessModel = (data, userOABs) => {
+  const getPerson = (personData) => ({
+    name: personData?.nome,
+    type: personData?.tipo,
+    isPJ: !!personData?.cnpj,
+    typePerson: personData?.tipo_pessoa,
+    document: !!personData?.cnpj ? personData?.cnpj : personData?.cpf,
+  });
+
+  return data.map((item) => {
+    const [tribunal] = item?.fontes;
+    const peopleInvolved = tribunal?.envolvidos.map((item) => getPerson(item));
+
+    const lawyers = peopleInvolved.filter(
+      (item) => item?.type?.toLowerCase() === "advogado"
+    );
+
+    const clients = tribunal?.envolvidos
+      .filter((item) => {
+        return item?.advogados?.some((advogado) => {
+          return userOABs?.some(({ number }) =>
+            advogado?.oabs?.some(({ numero }) => numero == number)
+          );
+        });
+      })
+      .map((item) => getPerson(item));
+
+    return {
+      clients,
+      lawyers,
+      peopleInvolved,
+      cnj: item?.numero_cnj,
+      coverJson: tribunal?.capa,
+      courtName: tribunal?.descricao,
+      plaintiff: item?.titulo_polo_ativo,
+      defendant: item?.titulo_polo_passivo,
+      archived: item?.fontes_tribunais_estao_arquivadas,
+      amount: Number(tribunal?.capa?.valor_causa?.valor),
+    };
+  });
+};
+
+const registerClients = async (processes, userRef) => {
+  try {
+    const data = processes.map(({ clients }) => clients).flat();
+    const clients = removeDuplicate(data, "document");
+
+    for await (let client of clients) {
+      const clientDTO = {
+        user: userRef,
+        status: "NOVO",
+        isActive: true,
+        isPJ: client.isPJ,
+        name: client.name,
+        isFavorite: false,
+        cpf: !client.isPJ ? client.document : "",
+        cnpj: client.isPJ ? client.document : "",
+
+        rg: "",
+        cep: "",
+        sex: "",
+        city: "",
+        state: "",
+        address: "",
+        pisNumber: "",
+        complement: "",
+        profession: "",
+        fantasyName: "",
+        observation: "",
+        nationality: "",
+        birthDate: null,
+        neighborhood: "",
+        maritalStatus: "",
+        addressNumber: "",
+        corporateReason: "",
+        issuingAuthority: "",
+        stateRegistration: "",
+        municipalRegistration: "",
+        emails: [{ title: "", email: "" }],
+        phones: [{ number: "", title: "" }],
+        companyRepresentative: [
+          {
+            cep: "",
+            cpf: "",
+            name: "",
+            city: "",
+            state: "",
+            email: "",
+            address: "",
+            complement: "",
+            observation: "",
+            birthDate: null,
+            phoneNumber: "",
+            neighborhood: "",
+            addressNumber: "",
+          },
+        ],
+      };
+
+      await admin.firestore().collection("Client").add(clientDTO);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const registerProcesses = async (processes, userRef) => {
+  try {
+    const clients = await getCollectionByUser("Client", userRef);
+
+    const getClientRef = (person) => {
+      const dbClientDocumentKey = person.isPJ ? "cnpj" : "cpf";
+      const dbClient = clients.find(
+        (client) =>
+          client.name === person.name &&
+          client[dbClientDocumentKey] === person.document
+      );
+
+      if (dbClient) return db.collection("Client").doc(dbClient.id);
+      else return null;
+    };
+
+    for await (let process of processes) {
+      const clients = process.clients
+        .map((person) => getClientRef(person))
+        .filter((item) => !!item);
+
+      const processDTO = { ...process, user: userRef, clients };
+      await admin.firestore().collection("Process").add(processDTO);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const registerIntimationMonitoring = async (intimations) => {
+  const { api_v1, api_token } = await getConfig();
+
+  intimations.forEach(({ registerProcessId }) => {
+    handleApiCall(`${api_v1}/monitoramentos`, api_token, "post", {
+      tipo: "processo",
+      processo_id: registerProcessId,
+    })
+      .then(() => {})
+      .catch(() => {
+        logger.info(
+          `+++ intimation Monitoring  (PROCESS: ${process.cnj}) +++`,
+          error,
+          { structuredData: true }
+        );
+      });
+  });
+};
+
+const registerIntimation = async (userRef) => {
+  const result = [];
+  const dbProcesses = [];
+
+  const { api_v1, api_token } = await getConfig();
+
+  const dbProcessesData = await db
+    .collection("Process")
+    .where("user", "==", userRef)
+    .where("archived", "==", false)
+    .get();
+
+  dbProcessesData.docs.forEach((doc) => {
+    const data = doc.data();
+    dbProcesses.push({ id: doc.id, ...data });
+  });
+
+  for await (let process of dbProcesses) {
+    try {
+      const processResponse = await handleApiCall(
+        `${api_v1}/processos/numero/${process.cnj}`,
+        api_token
+      );
+
+      const [{ id }] = processResponse.data;
+
+      const { data } = await handleApiCall(
+        `${api_v1}/processos/${id}/movimentacoes?limit=1`,
+        api_token
+      );
+
+      const [intimation] = data.items;
+
+      const dbProcessRef = db.collection("Process").doc(process.id);
+      const dbProcessData = (await dbProcessRef.get()).data();
+
+      const intimationDTO = {
+        user: userRef,
+        process: dbProcessRef,
+        clients: dbProcessData.clients,
+
+        type: intimation.tipo,
+        date: intimation.data,
+        registerId: intimation.id,
+        section: intimation.secao,
+        subType: intimation.subtipo,
+        snippet: intimation.snippet,
+        content: intimation.conteudo,
+        complement: intimation.complemento,
+        subProccess: intimation.subprocesso,
+        categoryText: intimation.texto_categoria,
+        registerProcessId: intimation.processo_id,
+
+        processResume: {
+          cnj: dbProcessData.cnj,
+          courtName: dbProcessData.courtName,
+          plaintiff: dbProcessData.plaintiff,
+          defendant: dbProcessData.defendant,
+          peopleInvolved: dbProcessData.peopleInvolved,
+        },
+        peopleInvolved: intimation.envolvidos.map((item) => ({
+          oab: item.oab,
+          name: item.nome,
+          registerId: item.id,
+          lawyerFor: item.advogado_de,
+          typeObject: item.objeto_type,
+          typeInvolved: item.envolvido_tipo,
+        })),
+      };
+
+      await dbProcessRef.set({ registerId: id }, { merge: true });
+      await admin.firestore().collection("Intimation").add(intimationDTO);
+
+      result.push(intimationDTO);
+    } catch (error) {
+      logger.info(`+++ intimation (PROCESS: ${process.cnj}) +++`, error, {
+        structuredData: true,
+      });
+    }
+  }
+
+  return result;
+};
+//#endregion utils
+
+// #region Database
+const getCollectionData = async (collectionName) => {
+  const snapshot = await db.collection(collectionName).get();
+  const result = [];
+
+  snapshot.forEach((doc) => {
+    const data = doc.data();
+    result.push({ id: doc.id, ...data });
+  });
+
+  return result;
+};
+
+const getCollectionByUser = async (collectionName, userRef) => {
+  const snapshot = await db
+    .collection(collectionName)
+    .where("user", "==", userRef)
+    .get();
+
+  const result = [];
+
+  snapshot.forEach((doc) => {
+    const data = doc.data();
+    result.push({ id: doc.id, ...data });
+  });
+
+  return result;
+};
+
+// #endregion Database
+
+exports.equalizeUserBase = onRequest(
+  { timeoutSeconds: 380 },
+  async (request, response) => {
+    logger.info("+++ INIT EQUALIZE USER BASE +++", { structuredData: true });
+
+    try {
+      let data = [];
+      const isDevMode = !!request?.body?.isDevMode;
+      const userRef = db.collection("User").doc(request.body.userId);
+      const userDocuments = await getCollectionByUser("UserOAB", userRef);
+
+      if (isDevMode) data = equilizeUserBaseMock;
+      else data = await buildUserProcessList(userDocuments);
+
+      const processes = buildUserProcessModel(data, userDocuments);
+
+      await registerClients(processes, userRef);
+      await registerProcesses(processes, userRef);
+
+      axios.post(
+        "https://equalizeUserIntimationBase-fp76zcdymq-uc.a.run.app",
+        request?.body
+      );
+
+      response.json({
+        message: "success",
+        data: {
+          allProcessesCount: data.length,
+          activeProcessesCount: processes.length,
+        },
+      });
+    } catch (error) {
+      logger.error("+++ ERROR EQUALIZE USER BASE +++", error, {
+        structuredData: true,
+      });
+
+      response.status(400).json({ message: "error", error });
+    }
+  }
+);
+
+exports.equalizeUserIntimationBase = onRequest(
+  { timeoutSeconds: 540 },
+  async (request, response) => {
+    logger.info("+++ INIT equalizeUserIntimationBase +++", request.body, {
+      structuredData: true,
+    });
+
+    try {
+      const userRef = db.collection("User").doc(request.body.userId);
+      const result = await registerIntimation(userRef);
+
+      const intimationMonitoring = result.slice(0, 200);
+      registerIntimationMonitoring(intimationMonitoring);
+
+      response.json({
+        message: "success",
+        data: { count: intimationMonitoring.length },
+      });
+    } catch (error) {
+      logger.error("+++ ERROR equalizeUserIntimationBase +++", error, {
+        structuredData: true,
+      });
+
+      response.status(400).json({ message: "error", error });
+    }
+  }
+);
+
+exports.callbackMonitoring = onRequest((request, response) => {
+  logger.info("+++ INIT MONITORING +++", request.body, {
+    structuredData: true,
+  });
+  response.send("callback monitoring works");
+});
+
+const puppeteer = require("puppeteer");
+exports.exportPdf = onRequest({ memory: "1GiB" }, async (request, response) => {
+  try {
+    console.log("+++ INIT EXPORT PDF +++", request.body);
+
+    response.set("Access-Control-Allow-Origin", "*");
+    response.set("Access-Control-Allow-Methods", "GET, POST");
+
+    if (request.method === "OPTIONS") {
+      response.status(204).send("");
+      return;
+    }
+
+    const { content, fileName } = request.body;
+    const isLocalHost = request.headers.host.includes("localhost");
+
+    const puppeteerConfig = {
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    };
+
+    if (!isLocalHost)
+      puppeteerConfig.executablePath = puppeteer.executablePath();
+
+    const browser = await puppeteer.launch(puppeteerConfig);
+    const page = await browser.newPage();
+
+    await page.setContent(content, { waitUntil: "domcontentloaded" });
+    const pdfBuffer = await page.pdf({ format: "A4", landscape: false });
+
+    await browser.close();
+
+    response.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${fileName || "output"}.pdf"`
+    );
+    response.setHeader("Content-Type", "application/pdf");
+
+    response.send(pdfBuffer);
+  } catch (error) {
+    console.error("+++ [EXPORT PDF] +++", error);
+    response.status(500).json({
+      message: "Error",
+      data: error.message,
+    });
+  }
+});
+
+exports.updateProcessArchiving = onRequest(async (request, response) => {
+  try {
+    console.log("+++ INIT updateProcessArchiving +++", request.body);
+
+    const { archived, processId, userId } = request.body;
+    const { api_v1, api_v2, api_token } = await getConfig();
+
+    const dbUserRef = db.collection("User").doc(userId);
+    const dbProcessRef = db.collection("Process").doc(processId);
+    const dbProcessData = (await dbProcessRef.get()).data();
+
+    await dbProcessRef.update({ archived });
+    await admin
+      .firestore()
+      .collection("historicalProcesses")
+      .add({
+        userRef: dbUserRef,
+        cnj: dbProcessData.cnj,
+        processRef: dbProcessRef,
+        datetimeAction: new Date(),
+        typeAction: `Processo ${archived ? "arquivado" : "reativado"}`,
+      });
+
+    // const { data: processResponse } = await handleApiCall(
+    //   `${api_v2}/processos/numero_cnj/${dbProcessData.cnj}`,
+    //   api_token
+    // );
+
+    // if (!Object.keys(processResponse).length) {
+    //   response.status(404).json({
+    //     message: "Processo não encontrado",
+    //     data: {},
+    //   });
+    //   return;
+    // }
+
+    response.json({
+      message: "success",
+      data: dbProcessData,
+    });
+  } catch (error) {
+    console.error("+++ [updateProcessArchiving] +++", error);
+    response.status(500).json({
+      message: "Error",
+      data: error.message,
+    });
+  }
+});
